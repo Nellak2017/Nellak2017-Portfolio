bd672f42bd72159386e0413df5373ad17abc7943
Connor Keenum <Nellakprime@gmail.com>
2022-04-25 18:30:49 -0500

    Fixed the head element into a Head element. Learned you aren't supposed to put styles in the Head, but in the document.

--- nellak2017-portfolio/pages/index.js
+++ nellak2017-portfolio/pages/index.js
@@ -2,20 +2,22 @@ import Head from 'next/head'
 import Image from 'next/image'
 
 import Nav from '../components/nav/nav'
+import Intro from '../components/intro/intro'
 import Showcase from '../components/showcase/showcase'
 import Footer from '../components/footer/footer'
 
 export default function Home() {
   return (
     <>
-    <head>
+      <Head>
 
-    </head>
-    <main>
-      <Nav></Nav>
-      <Showcase></Showcase>
-      <Footer></Footer>
-    </main>
+      </Head>
+      <main>
+        <Nav />
+        <Intro></Intro>
+        <Showcase></Showcase>
+        <Footer></Footer>
+      </main>
     </>
   )
 }
