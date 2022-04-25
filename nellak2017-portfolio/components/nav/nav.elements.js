18b52b0378fe7273b155949e3ef48764c0f8753d
Connor Keenum <Nellakprime@gmail.com>
2022-04-25 18:27:29 -0500

    Styled my Nav bar just the way I like it.

--- nellak2017-portfolio/components/nav/nav.elements.js
+++ nellak2017-portfolio/components/nav/nav.elements.js
@@ -2,32 +2,55 @@ import styled from 'styled-components'
 import Link from 'next/link';
 
 export const Navbar = styled.nav`
-    background: rgb(82,94,101);
-    height: 48px;
-    width: 100%;
     display: flex;
     justify-content: center;
     align-items: center;
     text-align: center;
-    font-size: 1.1rem;
-    position: sticky;
+
+    padding: 0;
+
+    height: 48px;
+    width: 100%;
+    min-width: 20rem;
+
+    position: fixed;
+    overflow: hidden;
     top: 0;
-    z-index: 998;
+
+    background: rgb(82,94,101);
 
     box-shadow: 0px 0px 15px 0px rgba(0,0,0,0.75);
     -webkit-box-shadow: 0px 0px 15px 0px rgba(0,0,0,0.75);
     -moz-box-shadow: 0px 0px 15px 0px rgba(0,0,0,0.75);
 
-    & ul{
-        display: flex;
-        column-gap: 0.25rem;
-        height: 100%;
-        text-decoration: none;
-        list-style-type: none;
+    z-index: 998;
+`;
+
+export const NavbarContainer = styled.section`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    column-gap: 1rem;
+    
+    margin-right: auto;
+    margin-left: auto;
+    padding-right: 50px;
+    padding-left: 50px;
+
+    height: 100%;
+    width: 100%;
+    max-width: 1300px;
+
+    z-index: 999;
+
+    @media screen and (max-width: 991px){
+        padding-right: 30px;
+        padding-left: 30px;
     }
-    & li{
+
+    & a{
+        font-size: 100%;
         height: 100%;
-        padding: 0 15px;
         display: flex;
         justify-content: center;
         align-items: center;
@@ -35,31 +58,10 @@ export const Navbar = styled.nav`
         border-bottom: 4px solid transparent;
     }
 
-    & li:hover{
+    & a:hover{
         cursor: pointer;
         border-bottom: 4px solid rgb(115,204,168);
         color: rgb(115,204,168);
-    }
-`;
-
-export const NavbarContainer = styled.section`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    column-gap: 0.25rem;
-    height: 100%;
-
-    z-index: 999;
-    width: 100%;
-    max-width: 1300px;
-    margin-right: auto;
-    margin-left: auto;
-    padding-right: 50px;
-    padding-left: 50px;
-
-    @media screen and (max-width: 991px){
-        padding-right: 30px;
-        padding-left: 30px;
     }
 `;
 
