6825917d677d1697e1088dc39c0cebc25ea45b16
Connor Keenum <Nellakprime@gmail.com>
2022-04-25 18:28:36 -0500

    Fixed the Links.

--- nellak2017-portfolio/components/nav/nav.js
+++ nellak2017-portfolio/components/nav/nav.js
@@ -4,28 +4,18 @@ import {
     NavLink
 } from './nav.elements.js'
 
+// Note: If you were using a headless CMS, you would define a get static props to get the data used
+// Note: Since this is merely a simple Portfolio Page, you can hard code it yourself, and serve assets from cdn
 const Nav = () => {
     return (
         <>
             <Navbar>
                 <NavbarContainer>
-                    <ul>
-                        <li>
-                            <NavLink href="/">Home</NavLink>
-                        </li>
-                        <li>
-                            <NavLink href="/#Games">Tech Experience</NavLink>
-                        </li>
-                        <li>
-                            <NavLink href="/#Main Projects">Main Projects</NavLink>
-                        </li>
-                        <li>
-                            <NavLink href="/#About">About</NavLink>
-                        </li>
-                        <li>
-                            <NavLink href="/#Resume">Resume</NavLink>
-                        </li>
-                    </ul>
+                    <NavLink href="/">Home</NavLink>
+                    <NavLink href="/#Main Projects" forwardedAs="/#Main Projects">Main Projects</NavLink>
+                    <NavLink href="/#Tech Experience" forwardedAs="/#Tech Experience">Tech Experience</NavLink>
+                    <NavLink href="/#About" forwardedAs="/#About">About</NavLink>
+                    <NavLink href="/#Resume" forwardedAs="/#Resume">Resume</NavLink>
                 </NavbarContainer>
             </Navbar>
         </>
