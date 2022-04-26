ee116a10b61f6985c528919a246e21a1d68db8b3
Connor Keenum <Nellakprime@gmail.com>
2022-04-25 23:56:09 -0500

    Added new fonts.

--- nellak2017-portfolio/pages/_document.js
+++ nellak2017-portfolio/pages/_document.js
@@ -6,6 +6,7 @@ export default function Document() {
         <Html>
             <Head>
                 <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@100&display=swap" rel="stylesheet" />
+                <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@100&family=Titillium+Web:wght@400;900&display=swap" rel="stylesheet"></link>
             </Head>
             <body>
                 <Main />
