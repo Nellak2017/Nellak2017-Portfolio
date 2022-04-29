adb3b57a56bdc175407d32a8a5a49f66607639a6
Connor Keenum <Nellakprime@gmail.com>
2022-04-28 19:12:15 -0500

    Added a new paragraph element and modified it so it would display the background image properly.

--- nellak2017-portfolio/components/showcase/showcase.js
+++ nellak2017-portfolio/components/showcase/showcase.js
@@ -5,12 +5,12 @@ import {
     LinkContainer,
     ShowcaseLinkContainer,
     ShowcaseLink,
-    LinkText
+    LinkText,
+    ShowcaseParagraph
 } from './showcase.elements.js'
-import Logo from '../../public/Nellak2017-Logo.jpg' // used as a dummy picture for dev
 
 // I am selecting the Section by id to scroll to it. There may be better ways but I don't know them
-const Showcase = ({ title, subtitle, links }) => {
+const Showcase = ({ title, subtitle, links, paragraph }) => {
     return (
         <>
             <ShowcaseContainer id={`#${title}`}>
@@ -21,27 +21,37 @@ const Showcase = ({ title, subtitle, links }) => {
                     {subtitle}
                 </Subtitle>
                 <LinkContainer>
-                    {links.map((link, index) => {
+                    {links?.map((link, index) => {
                         return (
                             <ShowcaseLinkContainer key={`${title}_${index}`}>
-                                <ShowcaseLink 
-                                href={link.href} 
-                                alt={title}
-                                style={{
-                                    backgroundImage: `url(${Logo.src})`,
-                                    backgroundSize: "cover",
-                                    borderRadius: "50%",
-                                    backgroundColor: "rgba(34,39,51,0.5)"
-                                }}
+                                <ShowcaseLink
+                                    href={link.href}
+                                    alt={link.alt}
+                                    style={{
+                                        backgroundImage: `url(${link.logo.src})`,
+                                        backgroundSize: "65%",
+                                        borderRadius: "50%",
+                                        backgroundColor: "rgba(34,39,51,0.5)"
+                                    }}
                                 >
                                     <LinkText>
-                                        {link.text}
+                                        <b style={{fontSize: "1.25rem", fontWeight: "bold"}}>{link.text}</b>
+                                        <p>{link.text2}</p>
                                     </LinkText>
                                 </ShowcaseLink>
                             </ShowcaseLinkContainer>
                         );
                     })}
                 </LinkContainer>
+                <ShowcaseParagraph>
+                    {paragraph?.map((p, index) => {
+                        return (
+                            <p>
+                                {p.p1}
+                            </p>
+                        )
+                    })}
+                </ShowcaseParagraph>
             </ShowcaseContainer>
         </>
     );
