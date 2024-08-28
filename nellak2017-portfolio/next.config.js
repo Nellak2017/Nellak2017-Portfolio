42185b62b44bc4405fede79a8089a8a47e70956d
Connor Keenum <Nellakprime@gmail.com>
2024-08-28 18:43:06 -0500

    Update config to allow for Github Pages.

--- nellak2017-portfolio/next.config.js
+++ nellak2017-portfolio/next.config.js
@@ -1,31 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // Deploy a Next.js App to GitHub Pages: https://www.youtube.com/watch?v=mJuz45RXeXY
+  distDir: 'dist', // Deploy a Next.js App to GitHub Pages: https://www.youtube.com/watch?v=mJuz45RXeXY
   reactStrictMode: true,
   images: {
-    // cache optimized images for 60 seconds    
+    // cache optimized images for 60 seconds
+    unoptimized: true, // Deploy a Next.js App to GitHub Pages: https://www.youtube.com/watch?v=mJuz45RXeXY   
     minimumCacheTTL: 60,  // https://www.codemotion.com/magazine/frontend/optimize-next-js-for-production/
   },
   compiler: {
     // Enables the styled-components SWC transform
     styledComponents: true
   },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Cross-Origin-Opener-Policy",
-            value: "same-origin", // "same-origin-allow-popups"
-          },
-          {
-            key: "Cross-Origin-Embedder-Policy",
-            value: "require-corp",
-          }
-        ],
-      },
-    ]
-  }, // This eliminates the Google Sign-in / Sign-up error where it says the Cross Origin is disallowed
 }
 
 module.exports = nextConfig
