bb16cb89d8f92d7c361579ffb1b5c2a8e85168d4
Connor Keenum <Nellakprime@gmail.com>
2024-08-29 18:24:28 -0500

    Update basePath

--- Nellak2017-Portfolio/src/pages/_document.js
+++ Nellak2017-Portfolio/src/pages/_document.js
@@ -3,7 +3,8 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from "styled-components"
 // import { BASE_PATH } from '../data/constants.js' // STUPID FILE NAMEING STUFF WON'T LET ME IN LINUX ENV
 
-const BASE_PATH = process.env.NODE_ENV === 'production' ? '/Nellak2017-Portfolio' : '' // Do this because the github actions suck
+//const BASE_PATH = process.env.NODE_ENV === 'production' ? '/Nellak2017-Portfolio' : '' // Do this because the github actions suck
+const BASE_PATH = '' // the new domain isn't weird anymore
 const logoString = `${BASE_PATH}/Nellak2017-Logo.jpg`
 
 class MyDocument extends Document {
